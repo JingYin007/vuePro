dd5d61ba6e1ff2ea6589adebc4536c20b80cdea7
moTzxx <JingYin007@users.noreply.github.com>
2018-12-18 18:44:01 +0800

    ok
    ctrl+k

--- todolist/src/components/TodoItem.vue
+++ todolist/src/components/TodoItem.vue
@@ -1,16 +1,16 @@
 <template>
-  <li @click="handleClick">{{content}}</li>
+	<li @click="handleClick">{{content}}</li>
 </template>
 
 <script>
-export default {
-props:[ 'content', 'index'],
-methods:{
-'handleClick': function () {
-this.$emit('delete', this.index);
-}
-}
-}
+	export default {
+		props: ['content', 'index'],
+		methods: {
+			'handleClick': function() {
+				this.$emit('delete', this.index);
+			}
+		}
+	}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
